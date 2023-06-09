960bb0bb51629bc4e0e79dd6eeb5e93d599de216
Mahaveer Mandloi <132800572+Mahaveermandloi@users.noreply.github.com>
2023-06-09 15:27:05 +0530

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,23 +6,22 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 
 
 const Home = () => {
-        return (
-                <>
-                    <div name="home" className='h-screen w-full
-                    bg-gradient-to-b from-black via-black to-gray-800 text-white '>
-        
-                        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row  '>
-        
-                            <div className='flex flex-col justify-center h-full'>
+    return (
+        <>
+            <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800 text-white '>
+                <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row  '>
 
-                        <h1 className='text-4xl sm:text-7xl font-bold text-white mt-20 '>I'm a Tech Explorer</h1>
+                    <div className='flex flex-col justify-center h-full
+                    '>
+                    
+                        <h2 className='text-4xl  sm:text-7xl font-bold text-white mt-20 '>I'm a Tech Explorer</h2>
 
                         <p className='text-gray-400 py-4  max-w-md'>
-                        An enthusiastic learner and aspiring technologist.With a passion for technology.I am constantly exploring these technologies to enhance my skills and expand my horizons.</p>
+                            An enthusiastic learner and aspiring technologist.With a passion for technology.I am constantly exploring these technologies to enhance my skills and expand my horizons.</p>
 
                         <div >
 
-                            <Link to="portfolio" smooth duration={500} className='group text-white  w-fit px-6 py-3  my-2 flex item-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
+                            <Link to="project" smooth duration={500} className='group text-white  w-fit px-6 py-3  my-2 flex item-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer'>
                                 Portfolio
 
                                 <span className='group-hover:rotate-90 duration-300'>
@@ -34,7 +33,7 @@ const Home = () => {
                     </div>
 
                     <div>
-                        <img src={anyimg} alt="myprofile" className='rounded-2xl mx-auto w-3/5  md:full' />
+                        <img src={anyimg} alt="myprofile" className='rounded-2xl mx-auto w-2/4  md:full' />
                     </div>
 
                 </div>
